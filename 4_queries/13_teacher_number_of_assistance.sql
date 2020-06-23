b7ac0aeb3a677176da8643c61cfa476fd475dfd1
briantran98 <98brian.tran@gmail.com>
2020-06-23 22:20:18 +0000

    Added queries for number of assignments and duration for each day along with how many assitances that teachers have provided

---
+++ 4_queries/13_teacher_number_of_assistance.sql
@@ -0,0 +1,8 @@
+SELECT teachers.name AS teacher, cohorts.name AS cohort, COUNT(assistance_requests.*) AS total_assistance
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = 'JUL02'
+GROUP BY teacher, cohort
+ORDER BY teacher;
