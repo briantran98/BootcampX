82cef9eac68b6b8e92a2ddecef39eeb6f7ad8b51
briantran98 <98brian.tran@gmail.com>
2020-06-25 01:25:48 +0000

    santizing user input before injecting into SQL

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
 SELECT teachers.name AS teacher, cohorts.name AS cohort
 FROM assistance_requests
 JOIN teachers ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 GROUP BY teacher, cohort
-ORDER BY teacher;`)
+ORDER BY teacher;
+`;
+
+const cohortName = process.argv[2] || 'JUL02';
+const values = [cohortName]
+
+pool.query(queryString, values)
 .then((res) => {
     console.log(res.rows)
 })
