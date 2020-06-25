58e52f7169e070348591c4eb36510b4754362631
briantran98 <98brian.tran@gmail.com>
2020-06-25 00:56:17 +0000

    Added SQL query in JS code

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,22 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+SELECT teachers.name AS teacher, cohorts.name AS cohort
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+GROUP BY teacher, cohort
+ORDER BY teacher;`)
+.then((res) => {
+    console.log(res.rows)
+})
+.catch(err => console.error('query error', err.stack));
